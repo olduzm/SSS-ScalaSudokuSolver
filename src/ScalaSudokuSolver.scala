df77c3ddf39f614ff3c1248fd3b4d92630f010fc
olduzm <olduzm@gmail.com>
2012-01-19 09:29:05 +0000

    Update src/ScalaSudokuSolver.scala

--- src/ScalaSudokuSolver.scala
+++ src/ScalaSudokuSolver.scala
@@ -5,13 +5,21 @@ object ScalaSudokuSolver {
   val cols = digits
   val box_r = List("ABC", "DEF", "GHI")
   val box_c = List("123", "456", "789")
-  val squares = stringify(cross, rows, cols).flatten
-  val unitlist = stringify(cross, rows, cols) :::
-                 stringify(crossR, cols, rows)
+  val squares = cross(rows, digits)
+  val unitlist = cols.toList.map(c => cross(rows.toList, List(c))) :::
+    rows.toList.map(r => cross(List(r), digits.toList)) :::
+    box_r.flatMap(rs => box_c.map(cs => cross(rs, cs)))
+  val units = squares.map(sq => (sq, unitlist.filter(_.contains(sq)))).toMap
+  val peers = squares.map(sq => (sq, units.apply(sq).flatten.distinct.filterNot(_ == sq))).toMap
 
+  def cross(x:String, y:String):List[String] = cross(x.toList, y.toList)
+  def cross[A,B](x:List[A], y:List[B]):List[String] = for (a <- x; b <- y) yield (stringify(a, b))
 
-  def cross[A](xs:List[A], ys:List[A]):List[List[(A, A)]] = ys.map(y => xs.map( x => (x, y) ))
-  def crossR[A](xs:List[A], ys:List[A]):List[List[(A, A)]] = ys.map(y => xs.map( x => (y, x) ))
+  def gridValues(grid:String) : Map[String, Char] = {
+    val chars = grid.toSeq
+    assert( chars.size == 81, {println("The puzzle given should have exactly 81 squares")} )
+    squares.zip(chars).toMap
+  }
 
   def main(args: Array[String]) {
     test()
@@ -20,11 +28,20 @@ object ScalaSudokuSolver {
   /*** Some unit tests ***/
   def test() {
     assert( squares.size == 81 )
-    println(unitlist)
-    //    assert( unitlist.size == 27 )
+    assert( unitlist.size == 27 )
+    assert( squares.forall( units.apply(_).size == 3 ))
+    assert( squares.forall( peers.apply(_).size == 20 ))
+    assert( units.apply("C2") == List(
+      List("A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2"),
+      List("C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"),
+      List("A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3")))
+    assert( peers.apply("C2") == List("A2", "B2", "D2", "E2", "F2", "G2", "H2", "I2",
+      "C1", "C3", "C4", "C5", "C6", "C7", "C8", "C9",
+      "A1", "A3", "B1", "B3"))
+    println ("All tests pass.")
+
   }
 
   /*** Other helper methods ***/
-  def stringify(fn: (List[Char], List[Char]) => List[List[(Char, Char)]], x:String, y:String) : List[List[String]] =
-    fn(x.toList, y.toList).map( t => t.map( c=> c._1 + "" + c._2 ) )
+  def stringify(a:Any, b:Any) : String = (a + "" + b)
 }
